5a19503a6e503db2bcfb4547a45d08717ab5be5b
1nonlyvinh <vincenth2898@tuckahoeschools.org>
2025-01-30 13:30:07 +0000

    finished ui

---
+++ script.js
@@ -0,0 +1,10 @@
+const workoutArray = [
+    { workout: 'Push Ups'},
+    { workout: 'Pullups'},
+    { workout: 'Cardio'},
+    { workout: 'Squats'},
+    { workout: 'Burpees'},
+    { workout: 'Dips'},
+    { workout: 'Planks'},
+
+]
