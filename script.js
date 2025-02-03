9bc153356757481c8c8250245ddf52014301697a
1nonlyvinh <vincenth2898@tuckahoeschools.org>
2025-02-03 00:58:54 +0000

    init commit

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 const exercises = [
-  { name: "Push-Up", met: "6.91" },
-  { name: "Pull-Up", met: "8.00" },
+  { name: "Push-Ups", met: "6.91" },
+  { name: "Pull-Ups", met: "8.00" },
 ];
 
 const exerciseDropDown = document.getElementById('exercise-selection');
 
+// populates exercise drop-down
 for (let i = 0; i < exercises.length; i++) {
     let option = document.createElement("option");
       option.text = exercises[i].name;
       option.value = exercises[i].met;
       exerciseDropDown.appendChild(option);
+}
+
+const duration = [
+  { length: "15 Minutes", number: .25},
+  { length: "30 Minutes", number: .50},
+  { length: "45 Minutes", number: .75},
+  { length: "60 Minutes", number: 1.00},
+];
+
+const durationDropDown = document.getElementById('duration-selection');
+
+for (let i = 0; i < duration.length; i++) {
+  let option = document.createElement("option");
+    option.text = duration[i].length;
+    option.value = duration[i].number;
+    durationDropDown.appendChild(option);
 }
